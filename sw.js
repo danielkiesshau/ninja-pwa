0187eafca0e6355ebd181abbf3bffcf1d408838b
Daniel Kiesshau <danielkiesshau@outlook.com>
2021-12-11 14:28:34 -0300

    chore(pwa): cache assets

--- sw.js
+++ sw.js
@@ -1,7 +1,31 @@
+const staticCacheName = 'site-static'
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+]
+
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      console.log('caching shell assets')
+      return cache.addAll(assets)
+    })
+  )
+
 })
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ...', event);
+})
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ...', event);
 })
